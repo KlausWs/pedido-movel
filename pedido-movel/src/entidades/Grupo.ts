c53a06807467a19cb5e4deab64700cd93d7b8fd2
augusto.scher <augusto.scher@senior.com.br>
2017-05-30 19:41:59 -0300

    quinto commit
    quinto commit

--- pedido-movel/src/entidades/Grupo.ts
+++ pedido-movel/src/entidades/Grupo.ts
@@ -2,5 +2,11 @@ export class Grupo {
   
   nome: string;
   codigo: string
+  imagem: string;
 
+     constructor(nome: string, codigo: string) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.imagem = '';
+    }
 }

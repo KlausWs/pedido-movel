6067d11a7bdec197554d0500572323ffd039e4a3
augusto.scher <augusto.scher@senior.com.br>
2017-06-23 08:41:50 -0300

    Adicionando cards a pagina inicial

--- pedido-movel/src/pages/grupos/GruposController.ts
+++ pedido-movel/src/pages/grupos/GruposController.ts
@@ -2,28 +2,37 @@ import { ClienteController } from './../cliente/ClienteController';
 import { Grupo } from './../../entidades/Grupo';
 import { GrupoService } from './../../services/GrupoService';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { ProdutosController } from '../produtos/ProdutosController';
 
 
 @Component({
   templateUrl: 'grupos.html'
 })
-export class GruposController { 
-  
+export class GruposController {
+
   grupoService: GrupoService;
   listaClientes: ClienteController;
   clientes: ClienteController;
 
-  constructor(public navCtrl: NavController, _grupoService: GrupoService) {
+  tab1Root: any = ClienteController;
+  tab2Root: any = ClienteController;
+  mySelectedIndex: number;
+
+
+  constructor(public navCtrl: NavController, _grupoService: GrupoService, navParams: NavParams) {
     this.grupoService = _grupoService;
+    if (!navParams.data.tabIndex) {
+      //this.mySelectedIndex = navParams.data.tabIndex || 0;
+    }
   }
 
   searchByGroup(grupo: Grupo) {
-    this.navCtrl.push(ProdutosController, {grupo: grupo});
+    this.navCtrl.push(ProdutosController, { grupo: grupo });
   }
 
   searchByText(ev: any) {
-    this.navCtrl.push(ProdutosController, {value: ev.target.value});
+    this.navCtrl.push(ProdutosController, { value: ev.target.value });
   }
 }
+

4d8c114378bcc3d7e7aa9abd5efdd32871eee3c0
Klaus W. Schaly <klausschaly@gmail.com>
2017-06-27 14:30:44 -0300

    Ajustes tela de cliente
    - Ajustes do layout da tela de cliente;
    - Removendo coluna de email do cliente.

--- pedido-movel/src/components/footer.ts
+++ pedido-movel/src/components/footer.ts
@@ -1,7 +1,7 @@
 import { ClienteController } from './../pages/cliente/ClienteController';
 import { ConsultaClienteController } from './../pages/consultacliente/ConsultaClienteController';
 import { NavController } from 'ionic-angular';
-import { Component, Injectable } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: "footer.html",
